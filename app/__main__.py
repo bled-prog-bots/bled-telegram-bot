a1149392242d08b8405da66874073dd8f04f8b01
Dmitriy Kim <61637662+iriswolf@users.noreply.github.com>
2024-08-04 23:20:03 +0300

    Завоз логов и файловых конфигов

--- app/__main__.py
+++ app/__main__.py
@@ -1,12 +1,14 @@
 import asyncio
 
-from app import bot
+from app import bot, logger_config
 from app.config import settings
 from app.libs.chatgpt import ChatGptAPI
 from app.libs.wiki_api import WikiAPI
 
 
 async def main():
+    logger_config.setup(settings.ENVIRONMENT)
+
     wiki_api = WikiAPI()
     chat_gpt = ChatGptAPI(
         settings.OPENAPI_MODEL,
--- app/logger_config.py
+++ app/logger_config.py
@@ -1,1 +1,11 @@
-import logging
+import json
+from logging.config import dictConfig
+
+
+def setup(app_env: str) -> None:
+    dict_config = json.load(
+        open(f'logging.{app_env}.json', 'r', encoding='utf-8')
+    )
+
+    # Да, я знаю что есть fileConfig, но камон, json глазу приятнее и удобнее
+    dictConfig(dict_config)
