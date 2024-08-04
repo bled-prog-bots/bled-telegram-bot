6af41a5849d6549034a920e55f354f7a205d5b0b
Dmitriy Kim <61637662+iriswolf@users.noreply.github.com>
2024-08-04 23:58:05 +0300

    Взяли на борт Sentry

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,7 @@
 import asyncio
+import logging
+
+import sentry_sdk
 
 from app import bot, logger_config
 from app.config import settings
@@ -7,8 +10,6 @@ from app.libs.wiki_api import WikiAPI
 
 
 async def main():
-    logger_config.setup(settings.ENVIRONMENT)
-
     wiki_api = WikiAPI()
     chat_gpt = ChatGptAPI(
         settings.OPENAPI_MODEL,
@@ -24,4 +25,16 @@ async def main():
 
 
 if __name__ == '__main__':
+    logger_config.setup(settings.ENVIRONMENT)
+
+    if settings.SENTRY_DSN.get_secret_value():
+        logging.info('Sentry init')
+        sentry_sdk.init(
+            server_name='bled_tg_bot',
+            dsn=settings.SENTRY_DSN.get_secret_value(),
+            environment=settings.ENVIRONMENT,
+            traces_sample_rate=settings.SENTRY_TRACES_RATE,
+            profiles_sample_rate=settings.SENTRY_PROFILES_RATE
+        )
+
     asyncio.run(main())
--- app/config.py
+++ app/config.py
@@ -5,12 +5,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    ENVIRONMENT: Literal['dev', 'prod']
+    ENVIRONMENT: Literal['dev', 'prod'] = 'dev'
     BLED_CHAT_ID: int
     BOT_TOKEN: SecretStr
     OPENAPI_MODEL: str
     OPENAI_API_KEY: SecretStr
     OPENAI_CHAT_CONTEXT: str
+    SENTRY_DEBUG: bool
+    SENTRY_DSN: SecretStr
+    SENTRY_TRACES_RATE: float
+    SENTRY_PROFILES_RATE: float
 
     model_config = SettingsConfigDict(env_file='.env')
 
